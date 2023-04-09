b8ec8726eafc1ca309728207cfcc17a1b9a62894
Tamirat Dejene <129655588+tamirat-dejene@users.noreply.github.com>
2023-04-09 09:04:19 +0300

    Add files via upload
    Has made use of pointers.

---
+++ Project I Problem analysis source code 3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << "Side1" << '\t' << "Side2" << '\t' << "Hypot" << endl;
+    int coun = 0;
+    int i, j, k;
+    int *side1, *side2, *hyp; // side 1 - i
+    side1 = &i;               // side 2 - j
+    side2 = &j;               // hypotenuse - k
+    hyp = &k;
+
+    for (i = 1; i <= 100; ++i)
+    {
+        for (j = 1; j <= 100; ++j)
+        {
+            for (k = 1; k <= 100; ++k)
+            {
+                if (pow(*side1, 2) + pow(*side2, 2) == pow(*hyp, 2))
+                {
+                    cout << setw(5) << *side1 << '\t' << setw(5) << *side2 << '\t' << setw(5) << *hyp;
+                    cout << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
