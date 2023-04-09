3a4ecd87b7781b32d6bbc26dd56d80932fe1b2e1
teklu moges <123808771+teklumo@users.noreply.github.com>
2023-04-09 09:31:24 +0300

    Update pattern printing - j.cpp

--- pattern printing - j.cpp
+++ pattern printing - j.cpp
@@ -1,32 +1,32 @@
 #include<iostream>
-#include<stdlib.h>
 using namespace std;
 
 int main(){
 	int n;
-	cout<<"enter n:";   // you can try with 8 
+	a:
+  cout << "Enter the size: ";   // you can try with 8 
 	cin>>n;
 	if(n<=0){
-		exit(0);
+		goto a;
 	}
-  for(int i=0;i<n;i++){
+  for(int i=0;i<(n/2);i++){
     for(int j=0;j<=i;j++){
       cout<<char(j+65);
     }
-    for(int k=n;k>i;k--){
+    for(int k=(n/2);k>i;k--){
       cout<<"  "; 
     }
     for(int m=i;m>=0;m--){
       cout<<char(m+65);
     }
     cout<<endl;
-    if(i==n-1){
+    if(i==(n/2)-1){
 
-    for(int k=0;k<n;k++){
+    for(int k=0;k<(n/2);k++){
       cout<<char(k+65);
     }
-    for(int k=n+1;k>=0;k--){
-    	if(k==(n)){
+    for(int k=(n/2)+1;k>=0;k--){
+    	if(k==((n/2))){
 			continue;
 		}
       cout<<char(k+65);
@@ -35,14 +35,14 @@ int main(){
 	}
   }
   
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n-i;j++){
+  for(int i=0;i<(n/2);i++){
+    for(int j=0;j<(n/2)-i;j++){
       cout<<char(j+65);
     }
     for(int k=0;k<=i;k++){
       cout<<"  ";
     }
-    for(int m=(n-1)-i;m>=0;m--){
+    for(int m=((n/2)-1)-i;m>=0;m--){
       cout<<char(m+65);
     }
     cout<<endl;
