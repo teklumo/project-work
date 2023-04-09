f2e464652c6d91c1f7a72130dfc755d7413250a4
teklu moges <123808771+teklumo@users.noreply.github.com>
2023-04-09 09:14:42 +0300

    Update Pattern printing - i.cpp

--- Pattern printing - i.cpp
+++ Pattern printing - i.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 int main(){
 	int n;
-	cout<<"enter n:";
+	cout << "Enter the size: ";
 	cin>>n;
 	if(n<=0){
 		exit(0);
 	}
-  for(int i=0;i<n;i++){
+  for(int i=0;i<n/2;i++){
     for(int k=0;k<i;k++){
       cout<<"  ";
     }
-    for(int j=(n-1)-i;j>=0;j--){
+    for(int j=((n/2)-1)-i;j>=0;j--){
       cout<<char(j+65)<<" ";
     }
-    for(int m=1;m<n-i;m++){
+    for(int m=1;m<(n/2)-i;m++){
       cout<<char(m+65)<<" ";
     }
     cout<<endl;
   }
-  for(int i=0;i<(n-1);i++){
-    for(int k=(n-2)-i;k>0;k--){
+  for(int i=0;i<((n/2)-1);i++){
+    for(int k=((n/2)-2)-i;k>0;k--){
       cout<<"  ";
     }
     for(int j=i+1;j>=0;j--){
