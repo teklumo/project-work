d5013d7578b9df39c0946b850175c527a8801c75
teklu moges <123808771+teklumo@users.noreply.github.com>
2023-04-09 11:02:01 +0300

    Update Project I Problem analysis source code 3.cpp

--- Project I Problem analysis source code 3.cpp
+++ Project I Problem analysis source code 3.cpp
@@ -28,3 +28,22 @@ int main()
     }
     return 0;
 }
+// without pointer we can do it like this
+/*
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  cout<<"side1"<<'\t'<<"side2"<<'\t'<<"hypotnenuse"<<endl;
+      for(int i=1;i<=100;i++){
+        for(int j=1;j<=100;j++){
+          for(int k=1;k<=100;k++){
+            if(pow(i,2)+pow(j,2)==pow(k,2)){
+              cout<<" "<<i<<'\t'<<" "<<j<<'\t'<<" "<<k<<endl;
+            }
+          }
+        }
+      }
+  return 0;
+}*/
