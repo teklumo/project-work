34e7d63adc2599c50973254b27c80e789c016dfa
teklu moges <123808771+teklumo@users.noreply.github.com>
2023-04-10 14:41:37 +0300

    Update Pattern printing - i.cpp

--- Pattern printing - i.cpp
+++ Pattern printing - i.cpp
@@ -15,13 +15,9 @@ a:
     cout << "You entered invalid size.Please enter a positive integer!\n";
     goto a;
   }
-  if (n % 2 == 0)
+   if (n % 2 != 0)
   {
-    n = n;
-  }
-  else
-  {
-    n = n + 2;
+  n = n + 2;
   }
   for (int i = 0; i < n / 2; i++)
   {
