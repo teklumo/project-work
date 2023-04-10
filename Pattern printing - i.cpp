551f8798d9915dd756e85de735116531ea74b062
teklu moges <123808771+teklumo@users.noreply.github.com>
2023-04-10 11:57:06 +0300

    Update Pattern printing - i.cpp

--- Pattern printing - i.cpp
+++ Pattern printing - i.cpp
@@ -1,4 +1,67 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+a:
+  cout << "Enter the size of the pattern: ";
+  cin >> n;
+
+  cout << "\n*** THE SHAPE OF YOUR PATTERN IS ***\n";
+  cout << "-------------------------------------\n\n";
+
+  if (n <= 0)
+  {
+    cout << "You entered invalid size.Please enter a positive integer!\n";
+    goto a;
+  }
+  if (n % 2 == 0)
+  {
+    n = n;
+  }
+  else
+  {
+    n = n + 2;
+  }
+  for (int i = 0; i < n / 2; i++)
+  {
+    for (int k = 0; k < i; k++)
+    {
+      cout << "  ";
+    }
+    for (int j = ((n / 2) - 1) - i; j >= 0; j--)
+    {
+      cout << char(j + 65) << " ";
+    }
+    for (int m = 1; m < (n / 2) - i; m++)
+    {
+      cout << char(m + 65) << " ";
+    }
+    cout << endl;
+  }
+  for (int i = 0; i < ((n / 2) - 1); i++)
+  {
+    for (int k = ((n / 2) - 2) - i; k > 0; k--)
+    {
+      cout << "  ";
+    }
+    for (int j = i + 1; j >= 0; j--)
+    {
+      cout << char(j + 65) << " ";
+    }
+    for (int m = 1; m <= i + 1; m++)
+    {
+      cout << char(m + 65) << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
+//Second method of implementation
+
+/*
+#include <iostream>
 #include <cmath>
 using namespace std;
 int main()
@@ -49,66 +112,7 @@ int main()
     }
     return 0;
 }
-
-
-//Second method of implementation
-/*
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  int n;
-a:
-  cout << "Enter the size of the pattern: ";
-  cin >> n;
-  
-  cout<<"\n*** THE SHAPE OF YOUR PATTERN IS ***\n";
-  cout<<"-------------------------------------\n\n";
-  
-  if (n <= 0)
-  {
-    cout<<"You entered invalid size.Please enter a positive integer!\n";
-    goto a;
-  }
-  for (int i = 0; i < n / 2; i++)
-  {
-    for (int k = 0; k < i; k++)
-    {
-      cout << "  ";
-    }
-    for (int j = ((n / 2) - 1) - i; j >= 0; j--)
-    {
-      cout << char(j + 65) << " ";
-    }
-    for (int m = 1; m < (n / 2) - i; m++)
-    {
-      cout << char(m + 65) << " ";
-    }
-    cout << endl;
-  }
-  for (int i = 0; i < ((n / 2) - 1); i++)
-  {
-    for (int k = ((n / 2) - 2) - i; k > 0; k--)
-    {
-      cout << "  ";
-    }
-    for (int j = i + 1; j >= 0; j--)
-    {
-      cout << char(j + 65) << " ";
-    }
-    for (int m = 1; m <= i + 1; m++)
-    {
-      cout << char(m + 65) << " ";
-    }
-    cout << endl;
-  }
-  return 0;
-}
 */
-
-
-
 
 
 
